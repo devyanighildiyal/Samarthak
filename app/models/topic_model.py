34bb42bdbf47d0b7aedf5217193ceeaf3564dd4f
Devyani <115294124+devyanighildiyal@users.noreply.github.com>
2024-06-30 23:59:12 +0530

    Update topic_model.py

--- app/models/topic_model.py
+++ app/models/topic_model.py
@@ -1,1 +1,10 @@
+import joblib
 
+# Load the LDA model and vectorizer
+lda_model = joblib.load('D:/mental_health_project/lda_model.joblib')
+vectorizer = joblib.load('D:/mental_health_project/vectorizer.joblib')
+
+def extract_topics(text):
+    X = vectorizer.transform([text])
+    topic_distribution = lda_model.transform(X)
+    return topic_distribution
