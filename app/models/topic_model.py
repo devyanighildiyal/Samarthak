19e041f07ae4765426980537b7dfacb8a758f7e2
Devyani <115294124+devyanighildiyal@users.noreply.github.com>
2024-07-01 00:30:44 +0530

    Update topic_model.py

--- app/models/topic_model.py
+++ app/models/topic_model.py
@@ -1,6 +1,5 @@
 import joblib
 
-# Load the LDA model and vectorizer
 lda_model = joblib.load('D:/mental_health_project/lda_model.joblib')
 vectorizer = joblib.load('D:/mental_health_project/vectorizer.joblib')
 
